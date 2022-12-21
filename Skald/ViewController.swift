e0aa970af2fe422df98d08ebce871c3c1e43652a
Alexander <a.pride.xxi@tutanota.com>
2022-12-21 12:05:00 +0300

    initial commit

---
+++ Skald/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skald
+//
+//  Created by Александр Прайд on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
